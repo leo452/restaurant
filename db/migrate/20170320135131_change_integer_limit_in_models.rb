ae2d4e606942927d76c709b6009114a3acc5b17b
Leonardo merchan <leonardo.merchan.1992@gmail.com>
2017-03-21 08:22:51 +0000

    cambio del limite int

--- db/migrate/20170320135131_change_integer_limit_in_models.rb
+++ db/migrate/20170320135131_change_integer_limit_in_models.rb
@@ -1,5 +1,5 @@
 class ChangeIntegerLimitInModels < ActiveRecord::Migration
   def change
-    change_column :models, :volumen_total, :integer, limit=> 8
+    change_column :models, :volumen_total, :integer, limit=> 10
   end
 end
